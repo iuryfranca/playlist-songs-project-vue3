ec7463b7f26c2fd67d8bbee9d50ae131ef006062
Iury Franca <iurygfranca@gmail.com>
2023-03-02 01:36:35 -0400

    feat: new component Navbar with link routers

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+import { RouterLink } from 'vue-router';
+
+<template>
+  <div class="container py-4 pb-8 flex items-center">
+    <nav class="flex justify-between w-full">
+      <h1 class="gap-2">
+        <RouterLink to="/">Playlists Songs</RouterLink>
+      </h1>
+      <div class="ml-auto text-sm gap-2 flex">
+        <button>Logout</button>
+        <RouterLink class="btn ml-auto text-sm" to="/signup">Signup</RouterLink>
+        <RouterLink class="btn ml-auto text-sm" to="/login">Login</RouterLink>
+      </div>
+    </nav>
+  </div>
+</template>
