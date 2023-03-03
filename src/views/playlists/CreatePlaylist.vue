dd01cc6ccad004c4ad270f9a0f4402a5fc460680
Iury Franca <iurygfranca@gmail.com>
2023-03-03 16:30:58 -0400

    feat: organize input file   ✨

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -10,7 +10,14 @@
     ></textarea>
     <div class="mt-8">
       <label style="color: #a9a9a9">Carregar imagem da playlist</label>
-      <input class="pl-0 mt-0" type="file" required placeholder="Título" />
+      <input
+        class="pl-0 mt-0"
+        type="file"
+        required
+        placeholder="Título"
+        @change="handleChange"
+      />
+      <div v-if="fileError" class="error">{{ fileError }}</div>
     </div>
 
     <div v-if="error" class="error">{{ error }}</div>
@@ -22,12 +29,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const title = ref('')
-const description = ref('')
-
-const error = ref('')
+const title = ref<string>('')
+const description = ref<string>('')
+const file = ref<any>(null)
+const error = ref<null | string>(null)
+const fileError = ref<null | string>(null)
 
 const handleSubmit = () => {
   console.log(title.value, description.value)
+}
+
+// allowed file types
+const typesFile = ['image/png', 'image/jpeg']
+
+const handleChange = (e: any) => {
+  const selected = e.target.files[0]
+
+  if (selected && typesFile.includes(selected.type)) {
+    file.value = selected
+    fileError.value = null
+  } else {
+    file.value = null
+    fileError.value = 'Por favor, selecione um arquivo de imagem PNG ou JPEG'
+  }
 }
 </script>
